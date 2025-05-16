24f4cb68bd7136d5d0de0c9d01e422d4bb601565
edsonoliveira <edsonbrasiloliveira3@gmail.com>
2025-05-16 11:33:54 -0300

    lista

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,16 @@
+//<reference typess="cypress" />;
+
+describe("Fucionalidade: Produtos", () => {
+  beforeEach(() => {
+    cy.visit("http://lojaebac.ebaconline.art.br/produtos/");
+  });
+
+  it("Deve selecionar um produto de lista", () => {
+    cy.get('.post-2559 > .product-block')
+      //.first()
+      //.last()
+      //.eq(2)
+      .click();
+      cy.get('#tab-title-description > a').should('contain', ("Descrição"))
+  });
+});
